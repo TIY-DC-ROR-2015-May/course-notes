624740a70db5111443e9f8d43c4ebf9eb17fe945
James Dabbs <jamesdabbs@gmail.com>
2015-05-26 11:34:48 -0400

    Can fill up the board

--- may/26/test.rb
+++ may/26/test.rb
@@ -5,14 +5,39 @@ require './player'
 require './tic_tac_toe'
 
 class TicTacTest < MiniTest::Test
-  def test_no_one_has_won_at_start
+  def test_new_game
     p1 = Player.new
     p2 = Player.new
 
     g = TicTacToe.new p1, p2
-    
+
     assert_equal nil, g.winner
     assert_equal p1, g.current_player
+    refute g.over? # assert !g.over?
+  end
+
+  def test_can_fill_board
+    p1 = Player.new
+    p2 = Player.new
+    g  = TicTacToe.new p1, p2
+
+    refute g.board_full?
+
+    # Make moves
+    (1..9).each do |square|
+      g.record_move square
+    end
+    # g.record_move 1
+    # g.record_move 2
+    # g.record_move 3
+    # g.record_move 4
+    # g.record_move 5
+    # g.record_move 6
+    # g.record_move 7
+    # g.record_move 8
+    # g.record_move 9
+
+    assert g.board_full?
   end
 
   def test_can_determine_winner
--- may/26/tic_tac_toe.rb
+++ may/26/tic_tac_toe.rb
@@ -4,9 +4,31 @@ class TicTacToe
     @o = player2
 
     @current_player = @x
+
+    @board = Array.new 9
   end
 
   def winner
+  end
+
+  def board_full?
+    # @board.each do |entry|
+    #   if entry.nil?
+    #     return false
+    #   end
+    # end
+    # return true
+
+    # @board.any? { |e| e.nil? }
+    !@board.include?(nil)
+  end
+
+  def record_move square
+    @board[square - 1] = :x
+  end
+
+  def over?
+    !winner.nil? || board_full?
   end
 
   def current_player
