9789a354b045eb51c04fadde258b77bd683838d0
James Dabbs <jamesdabbs@gmail.com>
2015-05-26 11:46:11 -0400

    Working current player switch

--- may/26/test.rb
+++ may/26/test.rb
@@ -13,6 +13,7 @@ class TicTacTest < MiniTest::Test
 
     assert_equal nil, g.winner
     assert_equal p1, g.current_player
+    assert_equal :x, g.symbol_for_current_player
     refute g.over? # assert !g.over?
   end
 
@@ -23,21 +24,29 @@ class TicTacTest < MiniTest::Test
 
     refute g.board_full?
 
-    # Make moves
     (1..9).each do |square|
       g.record_move square
     end
-    # g.record_move 1
-    # g.record_move 2
-    # g.record_move 3
-    # g.record_move 4
-    # g.record_move 5
-    # g.record_move 6
-    # g.record_move 7
-    # g.record_move 8
-    # g.record_move 9
 
     assert g.board_full?
+  end
+
+  def test_fills_in_right_letter
+    p1 = Player.new
+    p2 = Player.new
+    g  = TicTacToe.new p1, p2
+
+    # Both players play
+    g.record_move 5
+    g.record_move 1
+
+    # Can see both moves on board
+    expected_board = [
+      :o,  nil, nil,
+      nil,  :x, nil,
+      nil, nil, nil
+    ]
+    assert_equal expected_board, g.board
   end
 
   def test_can_determine_winner
--- may/26/tic_tac_toe.rb
+++ may/26/tic_tac_toe.rb
@@ -1,4 +1,6 @@
 class TicTacToe
+  attr_reader :board
+
   def initialize player1, player2
     @x = player1
     @o = player2
@@ -12,19 +14,26 @@ class TicTacToe
   end
 
   def board_full?
-    # @board.each do |entry|
-    #   if entry.nil?
-    #     return false
-    #   end
-    # end
-    # return true
-
-    # @board.any? { |e| e.nil? }
     !@board.include?(nil)
   end
 
   def record_move square
-    @board[square - 1] = :x
+    @board[square - 1] = symbol_for_current_player
+    
+    # toggle_current_player
+    if @current_player == @x # it's x's turn
+      @current_player = @o # now it's o's turn
+    else
+      @current_player = @x # not it's x's turn
+    end
+  end
+
+  def symbol_for_current_player
+    if @current_player == @x
+      return :x
+    else
+      return :o
+    end
   end
 
   def over?
