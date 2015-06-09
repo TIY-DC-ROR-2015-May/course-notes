2513f4ae68de73e0b73bc702d3b826a9ef0ad213
James Dabbs <jamesdabbs@gmail.com>
2015-06-09 11:14:42 -0400

    Add idea index

--- june/09/server.rb
+++ june/09/server.rb
@@ -1,6 +1,21 @@
 require 'sinatra/base'
+require 'json'
+require 'pry'
+
+Database = [
+  "Twitter for dolphins",
+  "Snapchat for snapping turtles"
+]
 
 class MyServer < Sinatra::Base
+
+  # List of all ideas
+  get '/idea_list' do
+    JSON.unparse Database
+  end
+
+  # -----
+
   get '/hi' do
     "Hello World!"
   end
@@ -10,7 +25,18 @@ class MyServer < Sinatra::Base
   end
 
   post "/hi" do
+    puts "I'm in the /hi POST handler"
     "Hello World!".reverse
+  end
+
+  get "/bad/echo" do
+    puts "What word? "
+    word = gets.chomp
+    "Your word was: #{word}"
+  end
+
+  delete "/echo/:word" do
+    params[:word]
   end
 end
 
