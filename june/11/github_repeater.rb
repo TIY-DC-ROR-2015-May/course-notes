10581d770a04b44401f30016a8ebded4fdbe9c76
James Dabbs <jamesdabbs@gmail.com>
2015-06-11 11:51:29 -0400

    Display repos

--- june/11/github_repeater.rb
+++ june/11/github_repeater.rb
@@ -1,10 +1,14 @@
 require 'sinatra/base'
 require 'pry'
 
+require 'httparty'
+
 class GithubRepeater < Sinatra::Base
   get '/user/:username' do
     @name = params[:username]
     @friends = ["jamesdabbs", "kylehill", "kwals"]
+    @repos = HTTParty.get("https://api.github.com/users/#{@name}/repos",
+        query: { per_page: 100 })
     erb :user_page
   end
 end
